8c6f20e54119cef12ee5fc0c490ef111fd555cfa
Zdenek Vojkuvka <zdenek.vojkuvka@ibillboard.com>
2020-08-26 13:44:34 +0200

    Database modul

--- src/database.js
+++ src/database.js
@@ -1,7 +1,48 @@
 const redis = require("redis");
 
 const client = redis.createClient({
-    port      : 10547,               // replace with your port
-    host      : 'redis-10547.c80.us-east-1-2.ec2.cloud.redislabs.com',        // replace with your hostanme or IP address
-    password : 'P7XOZmP8pNU79AtJPWrHxTqzB2gsR1wi'
+    port: 10547,
+    host: 'redis-10547.c80.us-east-1-2.ec2.cloud.redislabs.com',
+    password: 'P7XOZmP8pNU79AtJPWrHxTqzB2gsR1wi'
 });
+
+client.on("error", (error) => {
+    console.error(error);
+});
+
+
+module.exports = {
+    clearRedisValue: (req, res) => {
+        const key = req.params.key;
+        const resetValue = 0;
+        client.get(key, (err, reply) => {
+            if (reply) {
+                client.set(key, resetValue, function (err, success) {
+                    if (success)
+                        res.send(`Value was cleared`);
+                    else
+                        res.send(`Value wasn't cleared ${err}`);
+                });
+            }
+            else res.send(`Key ${key} doesn't exist`);
+        })
+    },
+
+    increaseRedisValue: (key, req, res) => {
+        if (req.body.hasOwnProperty(key)) {
+            client.get(key, (err, reply) => {
+                let reqCountValue = Number(req.body[key]);
+                let countValue = Number(reply);
+                countValue += reqCountValue;
+                client.set(key, countValue, redis.print);
+                res.send(`Value was increased by ${reqCountValue}`);
+            });
+        }
+    },
+
+    getRedisValue: (key, res) => {
+        client.get(key, (err, reply) => {
+            res.send("Current count value is: " + reply);
+        })
+    },
+};
--- src/index.js
+++ src/index.js
@@ -1,53 +1,32 @@
 const fs = require('fs');
-const redis = require("redis");
 const server = require('./server.js');
-
-
-const client = redis.createClient({
-    port: 10547,               // replace with your port
-    host: 'redis-10547.c80.us-east-1-2.ec2.cloud.redislabs.com',        // replace with your hostanme or IP address
-    password: 'P7XOZmP8pNU79AtJPWrHxTqzB2gsR1wi'
-});
-
-client.on("error", function (error) {
-    console.error(error);
-});
-let countValue;
+const db = require('./database.js');
 
 server.parseJson();
 server.start();
 
+/* Api POST - count value*/
 server.apiPost('/track', (req, res) => {
     saveLog(req);
-    res.send('Saved!');
-    if (req.body.hasOwnProperty('count')) {
-        client.get("count", function (err, reply) {
-            countValue = Number(reply);
-            countValue += req.body.count;
-            client.set("count", countValue, redis.print);
-        });
-    }
+    db.increaseRedisValue('count', req, res);
 });
 
-server.apiPost('/reset', (req, res) => {
-    countValue = 0;
-    client.set("count", countValue, redis.print);
-    res.send("Value was cleared")
+/* Api POST - clear value*/
+server.apiPost('/clear/:key', (req, res) => {
+    db.clearRedisValue(req, res)
 });
 
+/* Api GET - get count value*/
 server.apiGet('/count', (req, res) => {
-    client.get("count", function (err, reply) {
-        res.send("Current count value is: " + reply);
-    })
+    db.getRedisValue('count', res)
 });
-
 
 function saveLog(req) {
     const log = fs.createWriteStream('log.txt', {flags: 'a'});
     log.write(`${new Date().toLocaleString()}, Request body:${JSON.stringify(req.body)}\n`);
     log.end();
 
-    log.on('error', function (err) {
+    log.on('error',  (err) => {
         console.log(err);
     })
 }
